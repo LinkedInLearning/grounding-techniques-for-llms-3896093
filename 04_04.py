5fd68706b296900c7b1daa7aa499deb1cde5cdb9
Denys Linkov <12723001+Diophontine@users.noreply.github.com>
2024-07-21 02:22:38 +0000

    fixed ratio

--- 04_04.py
+++ 04_04.py
@@ -7,12 +7,12 @@ entailment_pipe = pipeline("text-classification", model="MoritzLaurer/mDeBERTa-v
 
 def calculate_results(generated_results,references):
 		for generated in generated_results:
+			results = bleu.compute(predictions=generated, references=references,max_order=2)
+			print("Bleu", results)
+
 			print("\nStatement: ", generated[0], "\nReference: ", references[0])
 			results = bertscore.compute(predictions=generated, references=references, lang="en")
 			print("Bert Score", results)
-
-			results = bleu.compute(predictions=generated, references=references,max_order=2)
-			print("Bleu", results)
 
 			results = rouge.compute(predictions=generated, references=references)
 			print("Rouge", results)
@@ -26,6 +26,3 @@ calculate_results(generated_results, references)
 generated_results = [["the number of cards in a standard deck of cards is 52"],  ["54 cards"], ["52 cards"]]
 references = ["52 cards in a standard deck of cards"]
 calculate_results(generated_results, references)
-
-
-
