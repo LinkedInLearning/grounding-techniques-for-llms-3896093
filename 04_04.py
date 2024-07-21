090808178249a6eecfec054a789f02b3617a6ecd
Denys Linkov <12723001+Diophontine@users.noreply.github.com>
2024-07-21 01:43:39 +0000

    changed examples

--- 04_04.py
+++ 04_04.py
@@ -5,21 +5,27 @@ bleu = load("bleu") # from paper https://aclanthology.org/P02-1040.pdf
 rouge = load("rouge") # from paper https://aclanthology.org/W04-1013.pdf
 entailment_pipe = pipeline("text-classification", model="MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7")
 
+def calculate_results(generated_results,references):
+		for generated in generated_results:
+			print("\nStatement: ", generated[0], "\nReference: ", references[0])
+			results = bertscore.compute(predictions=generated, references=references, lang="en")
+			print("Bert Score", results)
 
-predictions = [["the number of cards in a standard deck of cards is 52"],  ["54 cards"], ["52 cards"]]
+			results = bleu.compute(predictions=generated, references=references,max_order=2)
+			print("Bleu", results)
+
+			results = rouge.compute(predictions=generated, references=references)
+			print("Rouge", results)
+			result = entailment_pipe({'text':generated, 'text_pair': references})
+			print("NLI Entailment", result)
+
+generated_results = [["the cat sat"]]
+references = ["the cat sat on a little mat"]
+calculate_results(generated_results, references)
+
+generated_results = [["the number of cards in a standard deck of cards is 52"],  ["54 cards"], ["52 cards"]]
 references = ["52 cards in a standard deck of cards"]
+calculate_results(generated_results, references)
 
-for prediction in predictions:
-	print("\nStatement: ", prediction[0], "\nReference: ", references[0])
-	results = bertscore.compute(predictions=prediction, references=references, lang="en")
-	print("Bert Score", results)
-
-	results = bleu.compute(predictions=prediction, references=references)
-	print("Bleu", results)
-
-	results = rouge.compute(predictions=prediction, references=references)
-	print("Rouge", results)
-	result = entailment_pipe({'text':prediction, 'text_pair': references})
-	print("NLI Entailment", result)
 
 
